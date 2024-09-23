dfdb41733fe23d66a8d3bc1a934263e8a875cc3f
MeydhiXploit <meydhimeydhi@gmai.com>
2024-09-23 21:19:40 +0800

    learn java

---
+++ src/b2Projector/Variabel.java
@@ -0,0 +1,44 @@
+package b2Projector;
+
+public class Variabel {
+
+    public static void main(String[] args) {
+        String nama = "Meydhi Ari Nugroho";
+        int umur = 20;
+        String myHobby = "Turu";
+        String gender = "Laki-laki";
+        String tanggalLahir = "23-08-2024";
+
+        char golonganDarah = 'B';
+        double beratBadan = 61.2;
+        double tinggiBadan = 200;
+        String country = "Indonesia";
+        String agama = "Islam";
+
+        String alamat = "Praya";
+        String tempatLahir = "Sidoarjo";
+        String asalSekolah = "SMKN 1 PRAYA TENGAH";
+        String universitas = "ITS";
+        String peliharaan = "iguana";
+        String namaPeliharaan = "combet";
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Hobby : " + myHobby);
+        System.out.println("jenis kelamin : " + gender);
+        System.out.println("tanggal lahir : " + tanggalLahir);
+
+        System.out.println("golongan darah : " + golonganDarah);
+        System.out.println("berat badan : " + beratBadan);
+        System.out.println("tinggi badan : " + tinggiBadan);
+        System.out.println("negara : " + country);
+        System.out.println("agama : " + agama);
+
+        System.out.println("alamat : " + alamat);
+        System.out.println("tempat lahir : " + tempatLahir);
+        System.out.println("asal sekolah :" + asalSekolah);
+        System.out.println("kampus di : " + universitas);
+        System.out.println("peliharaan : " + peliharaan);
+        System.out.println("nama peliharaan : " + namaPeliharaan);
+
+    }
+}
